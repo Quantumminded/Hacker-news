f4a6cb61567bee196bb531bb75950f8332ffa41f
valeviit <valeviit@icloud.com>
2022-10-25 17:57:03 +0200

    Add  React Skeleton

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import Card from "./Card";
+import Pagination from "./Pagination";
+
+export default function Main() {
+return (
+    <>
+        <div>MAIN</div>
+        <SearchBar/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Pagination/>
+    </>
+)
+}
