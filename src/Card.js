080e039a5f0fe70b0b1dd26f43950d8cb6547eb0
AdaN6 <a_disney_da@hotmail.com>
2022-10-25 18:44:00 +0200

    added data json file

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,16 @@
 import React from "react";
+import data from "./data.json"
 
 export default function Card(){
+let { hits } = data;
+
     return (
-        <>
-            <div>CARD</div>
-        </>
+        <div>
+            <div className="main-container">CARD</div>
+            <div>
+                <title {hits.map((hit) => ( <h2>{hit.title}</h2>))} />
+                
+            </div>
+        </div>
     )
 }
--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import Card from "./Card";
 import Pagination from "./Pagination";
+import ReactDOM from "react-dom";
 
 export default function Main() {
 return (
