a82ab330b29e5863635493c9f96b03371043a706
Luca <lukkino31@gmail.com>
2022-10-27 13:56:26 +0200

    adding card with promps

---
+++ src/Cardluca.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./index.css";
+import "./card.css";
+
+
+export default function Card(promps) {
+  
+    
+  return (
+    <div className="card-main-container">
+      <div className="card-top">
+        <h2 className="card-title">{promps.art.title}</h2>
+        <h3 className="card-author">{promps.art.author}</h3>
+      </div>
+      <div className="news-card-footer">
+        <p className="news-card-footer-element">{promps.art.points}</p>
+        <p className="news-card-footer-element">{promps.art.num_comments}</p>
+      </div>
+      <div className="read-more-section">
+        <p className="read-more-text">read more  </p>
+      </div>
+    </div>
+  );
+}
--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,20 @@
 import React from "react";
 import SearchBar from "./SearchBar";
-import Card from "./Card";
+// import Card from "./Card";
+import Card from "./Cardluca";
 import Pagination from "./Pagination";
 import ReactDOM from "react-dom";
+import Data from "./data.json"
 
 export default function Main() {
+
+  let { hits } = Data;
 return (
   <>
     <div>MAIN</div>
     <SearchBar />
     <div className="news-body">
-      <Card />
-      <Card />
-      <Card />
+      {hits.map(hit=>(<Card art={hit} />))}
     </div>
     <Pagination />
   </>
