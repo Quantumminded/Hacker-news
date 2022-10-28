e2b57281e216f2b4c3062a757914a25ab2b5bf79
valeviit <valeviit@icloud.com>
2022-10-28 13:40:05 +0200

    add comment and likes icone. And fix the body css

--- src/Cardluca.js
+++ src/Cardluca.js
@@ -13,8 +13,8 @@ export default function Card (promps) {
         <h3 className="card-author">{promps.art.author}</h3>
       </div>
       <div className="news-card-footer">
-        <p className="news-card-footer-element">{promps.art.points}</p>
-        <p className="news-card-footer-element">{promps.art.num_comments}</p>
+        <p className="news-card-footer-element">{promps.art.points} ❤️</p>
+        <p className="news-card-footer-element"> {promps.art.num_comments} 💬</p>
       </div>
       <div className="read-more-section">
         <p className="read-more-text">read more  </p>
